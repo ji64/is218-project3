547efe00eabf9a0fb0bb352b05f7513015284e11
jakob <ji64@njit.edu>
2022-05-01 22:41:25 -0400

    fixed user_test

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -6,20 +6,20 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
 from sqlalchemy_serializer import SerializerMixin
+
 Base = declarative_base()
 
 location_user = db.Table('location_user', db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('location_id', db.Integer, db.ForeignKey('locations.id'))
-)
+                         db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                         db.Column('location_id', db.Integer, db.ForeignKey('locations.id'))
+                         )
 song_user = db.Table('song_user', db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('song_id', db.Integer, db.ForeignKey('songs.id'))
-)
+                     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                     db.Column('song_id', db.Integer, db.ForeignKey('songs.id'))
+                     )
 
 
-
-class Song(db.Model,SerializerMixin):
+class Song(db.Model, SerializerMixin):
     __tablename__ = 'songs'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300), nullable=True, unique=False)
@@ -41,7 +41,6 @@ class Location(db.Model, SerializerMixin):
     longitude = db.Column(db.String(300), nullable=True, unique=False)
     latitude = db.Column(db.String(300), nullable=True, unique=False)
     population = db.Column(db.Integer, nullable=True, unique=False)
-
 
     def __init__(self, title, longitude, latitude, population):
         self.title = title
@@ -68,11 +67,11 @@ class User(UserMixin, db.Model):
     registered_on = db.Column('registered_on', db.DateTime)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
     is_admin = db.Column('is_admin', db.Boolean(), nullable=False, server_default='0')
-    #songs = db.relationship("Song", back_populates="user", cascade="all, delete")
+    songs = db.relationship("Song", back_populates="user", cascade="all, delete")
     locations = db.relationship("Location",
-                    secondary=location_user, backref="users")
-    songs = db.relationship("Song",
-                    secondary=song_user, backref="users")
+                                secondary=location_user, backref="users")
+    #songs = db.relationship("Song",
+    #                        secondary=song_user, backref="users")
 
     def __init__(self, email, password, is_admin):
         self.email = email
