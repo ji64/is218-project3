25db3108ffd0783a7b8c4a68cd4dbb6fc485af5d
jakob <ji64@njit.edu>
2022-05-04 15:19:17 -0400

    attempting to fix secret key

--- app/config.py
+++ app/config.py
@@ -38,6 +38,6 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SESSION_COOKIE_SECURE = False
     DEBUG = True
-    #WTF_CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = False
     #WTF_CSRF_CHECK_DEFAULT = False
 
